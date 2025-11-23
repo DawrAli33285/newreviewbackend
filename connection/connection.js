713708c91444bce73e15127ce88c8cbe3cfe2932
DawrAli33285 <lemightyeagle@gmail.com>
2025-11-23 14:15:22 +0500

    new

--- connection/connection.js
+++ connection/connection.js
@@ -1,8 +1,8 @@
 const mongoose=require('mongoose')
 
-let connect=mongoose.connect(`mongodb://127.0.0.1/drikreviews`)
+// let connect=mongoose.connect(`mongodb://127.0.0.1/drikreviews`)
 
-// let connect=mongoose.connect('mongodb+srv://dawar:dawar@cluster0.7b9t5dx.mongodb.net/')
+let connect=mongoose.connect('mongodb+srv://dawar:dawar@cluster0.7b9t5dx.mongodb.net/')
 
 // let connect=mongoose.connect('mongodb+srv://dirkahlemeyer_db_user:1OFijZpRzKpqVbeW@cluster0.qpwvf8e.mongodb.net/')
 
